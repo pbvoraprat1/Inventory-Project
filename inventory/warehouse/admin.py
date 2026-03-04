b384dab857e800b8a80eae44a761a20c8cad70f9
pbvoraprat1 <puk.boong329@gmail.com>
2026-03-04 11:10:44 +0700

    create superuser

--- inventory/warehouse/admin.py
+++ inventory/warehouse/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Product, Warehouse, StockBalance, StockTransaction
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Warehouse)
+admin.site.register(StockBalance)
+admin.site.register(StockTransaction)
