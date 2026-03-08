6f6beca9c3d41bfe03b328507d561364c46b6260
pbvoraprat1 <puk.boong329@gmail.com>
2026-03-08 20:58:18 +0700

    make API soft del for products,url,API for stock balance

--- inventory/warehouse/urls.py
+++ inventory/warehouse/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductListAPIView, StockMovementAPIView
+from .views import ProductDetailAPIView, ProductListAPIView, StockMovementAPIView, StockbalanceAPIView
 
 urlpatterns = [
     path('stock-movements/', StockMovementAPIView.as_view(), name='api-stock-movement'),
     path('products/', ProductListAPIView.as_view(), name='api-product-list'),
+    path('products/<uuid:product_id>/', ProductDetailAPIView.as_view(), name='api-product-detail'),
+    path('products/<uuid:product_id>/<int:warehouse_id>/', StockbalanceAPIView.as_view(), name='api-stock-balance'),
+
 ]
--- inventory/warehouse/views.py
+++ inventory/warehouse/views.py
@@ -1,10 +1,12 @@
+from itertools import product
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ValidationError
 from .services import perform_stock_transaction
-from .serializers import StockmovementSerializer, ProductSerializer
-from .models import Product, Warehouse, StockTransaction
+from .serializers import StockBalanceSerializer, StockmovementSerializer, ProductSerializer
+from .models import Product, StockBalance, Warehouse, StockTransaction
 from rest_framework.permissions import IsAuthenticated
 
 class StockMovementAPIView(APIView):
@@ -39,3 +41,45 @@ class ProductListAPIView(APIView):
         products = Product.objects.filter(is_active=True)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ProductDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def put(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+            serializer = ProductSerializer(product, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response({"error": "ไม่พบสินค้า"}, status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+            product.is_active = False
+            product.save()
+            return Response({"message": "ลบสินค้าสำเร็จ"}, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response({"error": "ไม่พบสินค้า"}, status=status.HTTP_404_NOT_FOUND)
+    def patch(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+            serializer = ProductSerializer(product, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response({"error": "ไม่พบสินค้า"}, status=status.HTTP_404_NOT_FOUND)
+        
+class StockbalanceAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, product_id, warehouse_id):
+        try:
+            stock_balance = StockBalance.objects.get(product_id=product_id, warehouse_id=warehouse_id)
+            serializer = StockBalanceSerializer(stock_balance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except StockBalance.DoesNotExist:
+            return Response({"error": "ไม่พบยอดคงเหลือสำหรับสินค้านี้ในคลังนี้"}, status=status.HTTP_404_NOT_FOUND)
+           
